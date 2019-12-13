16323a349bf7e03b88cc52e540add916e74121f7
lenka <chroncova@seznam.cz>
2019-12-13 19:47:15 +0100

    lenka

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    klistata: [
+      {
+        lat: 49.206807, 
+        lng: 16.607908,
+        datum: new Date(2018, 10, 5),
+        
+      },
+      {
+        lat: 49.211577,
+        lng: 16.710495,
+        datum: new Date(2019, 6, 5)
+      },
+      {
+        lat: 49.215666,
+        lng: 16.710495,
+        datum: new Date(2019, 10, 15)
+      }
+    ]
   },
   mutations: {
   },
-  actions: {
-  },
-  modules: {
-  }
+  
 })
